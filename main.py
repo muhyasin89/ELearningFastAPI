4e53f98dbaadf8a82711c9c0465dfc10535a2508
Muhammad Yasin <muhammadyasin@polyglotengineer.local>
2021-08-24 23:13:51 +0700

    [feature/change_request_format]change text to json

--- main.py
+++ main.py
@@ -1,19 +1,41 @@
+import json
 import os
 from typing import Optional
 
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+
+from utils import request_cran
 
 app = FastAPI()
 
 
 @app.get("/")
-def read_root():
+async def read_root():
     return {"message": "Hello World"}
 
 
+@app.get("/package/")
+async def read_root(request: Request):
+    limit = request.query_params.get("limit")
+    insert = request.query_params.get("insert")
+
+    insert = insert if insert else False
+    limit = int(limit) if limit else 0
+
+    list_cran = request_cran()
+
+    result = {
+        "limit": limit,
+        "insert": insert,
+        "data": list_cran[:limit] if limit else list_cran,
+    }
+
+    return result
+
+
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
+async def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
 
---
+++ utils.py
@@ -0,0 +1,21 @@
+import requests
+
+
+def request_cran():
+    package = requests.get("https://cran.r-project.org/src/contrib/PACKAGES")
+    list_package = package.text.split("\n\n")
+
+    result_list = []
+    for item in list_package:
+        dict_default = {}
+        for new_item in item.replace(",\n", ",").split("\n"):
+            list_dict = new_item.split(":")
+
+            if len(list_dict) < 2:
+                for key, value in result_list[-1].items():
+                    dict_default[key] = value + list_dict[0]
+                result_list[-1] = dict_default
+            else:
+                dict_default[list_dict[0]] = list_dict[1]
+        result_list.append(dict_default)
+    return result_list
